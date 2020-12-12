6ed00c684a2ccae468b41408c7168be2a225e72c
MayurBhowad <mayurbhowad1995@gmail.com>
2020-12-12 15:01:19 +0530

    initial Commit

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'eucill666@gmail.com',
+      pass: 'Ambedkar@786'
+    }
+  },
+  // dbConnstring: 'mongodb+srv://admin:123@ds137760.mlab.com:37760/code4share',
+  dbConnstring: 'mongodb+srv://mayurB:mayur12345@cluster0.lxieb.mongodb.net/code4share?retryWrites=true&w=majority',
+  sessionKey: 'HaloCode4Share'
+}
